ea01c6392d56e181e45e0c89b272a0eae0b0aee0
UBB <ubb@example.com>
2016-11-21 10:35:02 +0900

    js syntax test

--- assets/js/syntax.js
+++ assets/js/syntax.js
@@ -5,5 +5,14 @@
       '<del>$1</del>'
     );
   }
+  
+   function paragraph(){
+    document.body.innerHTML = document.body.innerHTML.replace(
+      /((^[0-9]{3}). )/gim,
+      '<br><a name="$2"><\/a>\n\n### $1'
+    );
+  }
+  
   strikethrough();
+  paragraph();
 })();
